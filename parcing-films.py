4e215831a78bb0cab80b57917d2be5c0152cec30
MashaAvdulova <avdulova@gmail.com>
2025-03-17 21:41:43 +0300

    17032025

--- parcing-films.py
+++ parcing-films.py
@@ -6,20 +6,25 @@ def parsing_film():
     url = 'https://en.wikipedia.org/wiki/Lists_of_films#By_topic'
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
     response = requests.get(url, headers=headers)
-    films = ''
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        # content = soup.find('div', class_='mw-content-ltr mw-parser-output')
-        # start = content.find_all('h2', id='By_topic')
-        # films_content = content.find_next_siblings()
+        headers_item = soup.find_all('div', class_='mw-heading mw-heading3')
+        start = 0
+        for ind, item in enumerate(headers_item):
+            if item.find('h3')['id'] == 'Historical':
+                start = ind
+        for item in headers_item[start:]:
+                list_of_titles = item.next_sibling.next_sibling
+                for ul in list_of_titles.find_all('li'):
+                    title = ul.find('a').text
+                    link = ul.find('a')['href']
+                    print(f'Заголовок - {title}, ссылка - http://{link}')
+                if item.next_sibling.next_sibling.next_sibling.next_sibling['class'][1] =='nw-heading2':
+                    break
 
-        # print(films_content)
-    return films
 
+# def write_film_text (films: str):
+#     with open ('films.txt', 'w', encoding='utf-8') as file:
+#         text = file.write(films)
 
-def write_film_text (films: str):
-    with open ('films.txt', 'w', encoding='utf-8') as file:
-        text = file.write(films)
-
-films = parsing_film()
-write_film_text(films)
+parsing_film()
